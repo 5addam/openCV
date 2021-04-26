86cd200842cd5b8f8c31ab716d74ef94496d00cd
5addam <saddam.mughal@protonmail.com>
2021-04-26 15:55:53 +0500

    initial commit

---
+++ read.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+
+# reads image from the given path
+# img = cv.imread('images/rPhoto2.jpeg')
+
+# open img in a new window
+# cv.imshow("Image",img)
+
+# reading videos
+capture = cv.VideoCapture('videos/cats.mp4')
+
+# read video frame by frame
+while capture.isOpened:
+    isTrue, frame = capture.read()
+
+    # if letter 'd' is presded, stop the video
+    if cv.waitKey(20) & 0xFF == ord('q'):
+        break
+
+    elif isTrue: #frame is available
+        width = capture.get(cv.CAP_PROP_FRAME_WIDTH) #frame width
+        height = capture.get(cv.CAP_PROP_FRAME_HEIGHT) #frame height
+        
+        print(str(width))
+        # convert img to grayscale
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        #show frame
+        cv.imshow("Video", gray)
+    else: #frame not read properly || end of frames
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+# waits for a key to be pressed(delay)
+# cv.waitKey(0)
